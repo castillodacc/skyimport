6b6b986026257bfd67c93611b59c1a5baf04c887
emaspc93 <ema_spc93@hotmail.com>
2018-05-08 18:10:24 -0400

    Correccion de sidebar
    Arreglado el active del side bard y otros problemas menores.

--- resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
@@ -2,17 +2,17 @@
     <section class="sidebar">
         <ul class="sidebar-menu tree" data-widget="tree">
             <li class="header text-center">Menu de navegacion</li>
-            <li><a href="{{ url('/') }}"><i class='fa fa-dashboard'></i><span>Inicio</span></a></li>
+            <li class="{!! Request::is('/') ? 'active' : '' !!}"><a href="{{ url('/') }}"><i class='fa fa-dashboard'></i><span>Inicio</span></a></li>
             <hr>
-            <li><a href="{{ route('profile') }}"><i class='fa fa-user-circle'></i><span>Perfil</span></a></li>
+            <li class="{!! Request::is('perfil') ? 'active' : '' !!}"><a href="{{ route('profile') }}"><i class='fa fa-user-circle'></i><span>Perfil</span></a></li>
             <hr>
             @if(Auth::user()->role_id == 1)
-            <li><a href="{{ route('usuarios.index') }}"><i class='fa fa-users'></i> <span>Usuarios</span></a></li>
+            <li class="{!! Request::is('usuarios') ? 'active' : '' !!}"><a href="{{ route('usuarios.index') }}"><i class='fa fa-users'></i> <span>Usuarios</span></a></li>
             <hr>
-            <li><a href="{{ route('tracking.index') }}"><i class='fa fa-location-arrow'></i> <span>Administrar Trackings</span></a></li>
+            <li class="{!! Request::is('tracking') ? 'active' : '' !!}"><a href="{{ route('tracking.index') }}"><i class='fa fa-location-arrow'></i> <span>Administrar Trackings</span></a></li>
             <hr>
             @endif
-            <li><a href="{{ route('consolidados.index') }}"><i class='fa fa-arrows-h'></i> <span>@if(Auth::user()->role_id == 1) Administrar @else Mis @endif envios</span></a></li>
+            <li class="{!! Request::is('consolidados') ? 'active' : '' !!}"><a href="{{ route('consolidados.index') }}"><i class='fa fa-arrows-h'></i> <span>@if(Auth::user()->role_id == 1) Administrar @else Mis @endif envios</span></a></li>
             <hr>
         </ul>
     </section>
