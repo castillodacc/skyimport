44d7dd7383113e7951d2d00779522e711bf6c5c2
emaspc93 <ema_spc93@hotmail.com>
2018-08-30 15:29:50 -0400

    Arreglos a vista de register
    Arreglos en el radio de seleccion de checkbox.
    Arreglos al margin general de la vista y a las posiciones de los
    elementos.
    Arreglos al margin general de la vista y a las posiciones de los
    elementos..

--- resources/views/vendor/adminlte/auth/registerwithoutvue.blade.php
+++ resources/views/vendor/adminlte/auth/registerwithoutvue.blade.php
@@ -5,14 +5,12 @@
 @endsection
 
 @section('content')
-
-<body class="hold-transition register-page">
+<body class="hold-transition register-page" style="margin-top: -120px;">
     <div id="app" v-cloak>
         <div class="register-box">
             <div class="login-logo">
                 <img class="center-block" src="/img/skyimportglobal.png">
             </div><!-- /.login-logo -->
-
             @if (count($errors) > 0)
                 <div class="alert alert-danger">
                     <strong>Whoops!</strong> {{ trans('adminlte_lang::message.someproblems') }}<br><br>
@@ -23,7 +21,6 @@
                     </ul>
                 </div>
             @endif
-
             <div class="register-box-body">
                 <p class="login-box-msg">{{ trans('adminlte_lang::message.registermember') }}</p>
                 <form action="{{ url('/register') }}" method="post">
@@ -58,30 +55,26 @@
                         <span class="fa fa-unlock form-control-feedback"></span>
                     </div>
                     <div class="row">
-                        <div class="col-xs-1">
-                                <div class="checkbox_register icheck">
-                                    <label>
-                                        <input type="checkbox" name="terms" {{ old('terms') ? 'checked' : '' }}>
-                                    </label>
-                                </div>
+                        <div class="col-xs-2 checkbox_register icheck">
+                            <input type="checkbox" name="terms" {{ old('terms') ? 'checked' : '' }}>
                         </div><!-- /.col -->
-                        <div class="col-xs-6">
-                            <div class="form-group">
-                                <a class="btn btn-block btn-flat" data-toggle="modal" data-target="#termsModal">{{ trans('adminlte_lang::message.terms') }}</a>
-                            </div>
-                        </div><!-- /.col -->
-                        <div class="col-xs-4 col-xs-push-1">
-                            <button type="submit" class="btn btn-primary btn-block btn-flat"><span class="fa fa-send"></span> {{ trans('adminlte_lang::message.register') }}</button>
-                        </div><!-- /.col -->
-                        <div class="col-md-12">
-                            <p class="text-justify"><b>* Ley de Protección de Datos Personales:</b> "La autorización suministrada en el presente formulario faculta a U.S Cargo SKY para que dé a sus datos aquí recopilados el tratamiento señalado en la "Política de Privacidad para el Tratamiento de Datos Personales" de U.S Cargo SKY, el cual incluye, entre otras, el envío de información promocional, así como la invitación de eventos. El titular de los datos podrá, en cualquier momento, solicitar que la información sea modificada, actualizada o retirada de las bases de datos de U.S Cargo SKY.</p>
+                        <div class="form-group">
+                                <a class="btn" data-toggle="modal" style="margin-left: -10px; margin-top: -5px; " data-target="#termsModal">{{ trans('adminlte_lang::message.terms') }}</a>
+                            <div class="col-xs-4 pull-right">
+                                <button type="submit" class="btn btn-primary btn-block btn-flat"><span class="fa fa-send"></span> {{ trans('adminlte_lang::message.register') }}</button>
+                            </div><!-- /.col -->
                         </div>
-                    </div>
+                    </div>    
+                    <div class="row">
+                        <div class="col-xs-8">
+                         <a href="{{ url('/login') }}" class="text-center">{{ trans('adminlte_lang::message.membreship') }}</a>
+                        </div>
+                    </div>       
                 </form>
                 {{-- @include('adminlte::auth.partials.social_login') --}}
                 <div class="row">
-                    <div class="col-xs-6">
-                         <a href="{{ url('/login') }}" class="text-center">{{ trans('adminlte_lang::message.membreship') }}</a>
+                    <div class="col-md-12">
+                            <p class="text-justify" style="font-size: 12px;"><b>Ley de Protección de Datos Personales:</b> "La autorización suministrada en el presente formulario faculta a Deloitte para que dé a sus datos aquí recopilados el tratamiento señalado en la "Política de Privacidad para el Tratamiento de Datos Personales" de Deloitte, el cual incluye, entre otras, el envío de información promocional, así como la invitación de eventos. El titular de los datos podrá, en cualquier momento, solicitar que la información sea modificada, actualizada o retirada de las bases de datos de Deloitte.</p>
                     </div>
                 </div>
             </div><!-- /.form-box -->
